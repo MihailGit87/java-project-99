48bbbdd92fdeb0d76179f0cbcbf92abef7812708
mikhailvasilev <mall.87@mail.ru>
2024-02-20 09:38:51 +0300

    unlock User

--- src/main/java/hexlet/code/model/User.java
+++ src/main/java/hexlet/code/model/User.java
@@ -1,6 +1,7 @@
 package hexlet.code.model;
 
 import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
@@ -8,9 +9,6 @@ import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
@@ -20,7 +18,7 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
@@ -37,25 +35,24 @@ public class User implements UserDetails, BaseEntity {
 
     @Id
     @GeneratedValue(strategy = IDENTITY)
+    @EqualsAndHashCode.Include
     private long id;
 
     private String firstName;
 
     private String lastName;
 
-    @NotNull
     @Email
+    @Column(unique = true)
     private String email;
 
-    @NotBlank
-    @Size(min = 3)
     private String password;
 
     @CreatedDate
-    LocalDate createdAt;
+    private LocalDateTime createdAt;
 
     @LastModifiedDate
-    LocalDate updatedAt;
+    private LocalDateTime updatedAt;
 
     @OneToMany(mappedBy = "assignee", cascade = CascadeType.MERGE)
     private List<Task> tasks = new ArrayList<>();
--- src/test/java/hexlet/code/util/TestUtils.java
+++ src/test/java/hexlet/code/util/TestUtils.java
@@ -1,6 +1,12 @@
 package hexlet.code.util;
 
+import hexlet.code.model.Label;
+import hexlet.code.model.TaskStatus;
 import hexlet.code.model.User;
+import hexlet.code.model.Task;
+import hexlet.code.repository.LabelRepository;
+import hexlet.code.repository.TaskRepository;
+import hexlet.code.repository.TaskStatusRepository;
 import hexlet.code.repository.UserRepository;
 import net.datafaker.Faker;
 import org.instancio.Instancio;
@@ -10,6 +16,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.HashSet;
 
 @Component
 public class TestUtils {
@@ -23,7 +31,19 @@ public class TestUtils {
     private UserUtils userUtils;
 
     @Autowired
+    private TaskStatusUtils taskStatusUtils;
+
+    @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private TaskStatusRepository taskStatusRepository;
+
+    @Autowired
+    private LabelRepository labelRepository;
+
+    @Autowired
+    private TaskRepository taskRepository;
 
     @Bean
     public User generateUser() {
@@ -39,7 +59,60 @@ public class TestUtils {
     }
 
     @Bean
+    public TaskStatus generateTaskStatus() {
+        var slugAndName = faker.lorem().word();
+        return Instancio.of(TaskStatus.class)
+                .ignore(Select.field(TaskStatus::getId))
+                .ignore(Select.field(TaskStatus::getCreatedAt))
+                .supply(Select.field(TaskStatus::getSlug), () -> slugAndName)
+                .supply(Select.field(TaskStatus::getName), () -> slugAndName)
+                .create();
+    }
+
+    @Bean
+    public Label generateLabel() {
+        return Instancio.of(Label.class)
+                .ignore(Select.field(Label::getId))
+                .ignore(Select.field(Label::getCreatedAt))
+                .supply(Select.field(Label::getName), () -> faker.lorem().word())
+                .supply(Select.field(Label::getTasks), () -> new ArrayList<Task>())
+                .create();
+    }
+
+    @Bean
+    public Task generateTask() {
+        var task = Instancio.of(Task.class)
+                .ignore(Select.field(Task::getId))
+                .ignore(Select.field(Task::getCreatedAt))
+                .ignore(Select.field(Task::getAssignee))
+                .ignore(Select.field(Task::getTaskStatus))
+                .supply(Select.field(Task::getName), () -> faker.lorem().word())
+                .supply(Select.field(Task::getIndex), () -> faker.number().randomNumber())
+                .supply(Select.field(Task::getDescription), () -> faker.lorem().sentence())
+                .supply(Select.field(Task::getLabels), () -> new HashSet<Label>())
+                .create();
+
+        var assignee = generateUser();
+        userRepository.save(assignee);
+        task.setAssignee(assignee);
+
+        var taskStatus = generateTaskStatus();
+        taskStatusRepository.save(taskStatus);
+        task.setTaskStatus(taskStatus);
+
+        var label = generateLabel();
+        labelRepository.save(label);
+        task.getLabels().add(label);
+        label.getTasks().add(task);
+
+        return task;
+    }
+
+    @Bean
     public void clean() {
+        taskRepository.deleteAll();
         userRepository.deleteAll();
+        taskStatusRepository.deleteAll();
+        labelRepository.deleteAll();
     }
 }
